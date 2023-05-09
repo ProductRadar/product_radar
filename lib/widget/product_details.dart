b1328eaa3fd254aaa0d81d2d0087d083edc7b31a
Mathias Duus <mathias031001@gmail.com>
2023-05-09 10:10:18 +0200

    Better wrapping on the title and description

--- lib/widget/product_details.dart
+++ lib/widget/product_details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:product_radar/bin/product/product_lib.dart' as product;
 import 'package:product_radar/widget/favorite.dart';
 import 'package:product_radar/widget/rating.dart';
@@ -13,8 +12,6 @@ class ProductDetail extends StatefulWidget {
   ProductDetailState createState() => ProductDetailState();
 }
 
-//TODO: Do something to get the data required, maybe by utilizing the init function
-
 class ProductDetailState extends State<ProductDetail> {
   @override
   Widget build(BuildContext context) {
@@ -22,8 +19,9 @@ class ProductDetailState extends State<ProductDetail> {
       child: FutureBuilder(
         future: product.getProduct(widget.id),
         builder: (context, snapshot) {
+          // The logic
           if (snapshot.hasData) {
-            return Lul(snapshot.data);
+            return productDetails(snapshot.data);
           } else if (snapshot.hasError) {
             return Center(child: Text("${snapshot.error}"));
           }
@@ -35,7 +33,8 @@ class ProductDetailState extends State<ProductDetail> {
     );
   }
 
-  Widget Lul(data) {
+  /// The widget to be displayed
+  Widget productDetails(data) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.red),
       body: Column(
@@ -80,15 +79,18 @@ class ProductDetailState extends State<ProductDetail> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisSize: MainAxisSize.max,
                   children: [
-                    Text(
-                      data['product']['name'],
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 34,
-                        color: Color(0xff000000),
+                    Flexible(
+                      child: Text(
+                        data['product']['name'],
+                        textAlign: TextAlign.start,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 34,
+                          color: Color(0xff000000),
+                        ),
                       ),
                     ),
                     Favorite(id: widget.id),
@@ -102,13 +104,13 @@ class ProductDetailState extends State<ProductDetail> {
             width: 16,
           ),
           Card(
-            margin: EdgeInsets.all(4.0),
-            color: Color(0xffe0e0e0),
-            shadowColor: Color(0xff000000),
+            margin: const EdgeInsets.all(4.0),
+            color: const Color(0xffe0e0e0),
+            shadowColor: const Color(0xff000000),
             elevation: 1,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(4.0),
-              side: BorderSide(color: Color(0x4d9e9e9e), width: 1),
+              side: const BorderSide(color: Color(0x4d9e9e9e), width: 1),
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -134,7 +136,8 @@ class ProductDetailState extends State<ProductDetail> {
                 borderRadius: BorderRadius.circular(4.0),
                 side: const BorderSide(color: Color(0x4d9e9e9e), width: 1),
               ),
-              child: Row(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   const Text(
                     "Description: ",
@@ -142,17 +145,18 @@ class ProductDetailState extends State<ProductDetail> {
                     overflow: TextOverflow.clip,
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      // fontStyle: FontStyle.normal,
                       fontSize: 24,
-                      // color: Color(0xff000000),
                     ),
                   ),
-                  Text(
-                    data['product']['description'],
-                    textAlign: TextAlign.start,
-                    overflow: TextOverflow.clip,
-                    style: const TextStyle(
-                      fontSize: 24,
+                  Flexible(
+                    child: Text(
+                      data['product']['description'],
+                      maxLines: 10,
+                      textAlign: TextAlign.start,
+                      overflow: TextOverflow.clip,
+                      style: const TextStyle(
+                        fontSize: 24,
+                      ),
                     ),
                   )
                 ],
@@ -164,97 +168,3 @@ class ProductDetailState extends State<ProductDetail> {
     );
   }
 }
-
-/*
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-      ),
-      body: Center(
-        child: FutureBuilder<Map<String, dynamic>>(
-          future: product.getProduct(widget.id),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Container(
-                height: 500,
-                decoration:
-                BoxDecoration(borderRadius: BorderRadius.circular(20)),
-                margin: const EdgeInsets.all(5),
-                padding: const EdgeInsets.all(5),
-                child: Stack(
-                  children: [
-                    Card(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Expanded(
-                            child: Image.network(
-                              snapshot.data?["product"]["image"],
-                              fit: BoxFit.contain,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Container(
-                                  color: Colors.red,
-                                  alignment: Alignment.center,
-                                  child: const Text(
-                                    'Whoops!',
-                                    style: TextStyle(fontSize: 30),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                snapshot.data?["product"]["name"],
-                                style: const TextStyle(
-                                  fontSize: 34,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Spacer(),
-                              Favorite(id: widget.id),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Card(
-                      child: Rate(id: widget.id),
-                    ),
-                    Card(
-                      child: Row(
-                        children: [
-                          const Text(
-                            "Description: ",
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            snapshot.data?['product']['description'],
-                            style: const TextStyle(
-                              fontSize: 24,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            } else if (snapshot.hasError) {
-              return Center(child: Text("${snapshot.error}"));
-            }
-
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
-          },
-        ),
-      ),
-    );
-  }
-*/
-//}
