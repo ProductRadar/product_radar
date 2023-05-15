23880eadf82248acb8db31e05c508967640cc662
Setanna <joenhendrikhoelckhansen@gmail.com>
2023-05-15 09:16:46 +0200

    Made it look a bit better

--- lib/about.dart
+++ lib/about.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:product_radar/widget/custom_drawer.dart';
@@ -21,8 +22,9 @@ class _AboutPageState extends State<AboutPage> {
     try {
       await launchUrl(mailUrl as Uri);
     } catch (e) {
-      await Clipboard.setData(ClipboardData(text: email)).then((_){
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Email address copied to clipboard")));
+      await Clipboard.setData(ClipboardData(text: email)).then((_) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Email address copied to clipboard")));
       });
     }
   }
@@ -55,19 +57,26 @@ class _AboutPageState extends State<AboutPage> {
                   "Perspiciatis numquam ea corporis atque sequi quo ex. Voluptatum non similique eos. Sit aperiam doloremque nobis aut. "),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  const Text("Mail us at "),
-                  GestureDetector(
-                    onTap: () {
-                      email = "idk@idk.idk";
-                      launchMailClient();
-                    },
-                    child: const Text("idk@idk.idk", style: TextStyle(color: Colors.blue),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Flexible(
+                child: RichText(
+                    text: TextSpan(
+                  style: TextStyle(color: Colors.black),
+                  children: <TextSpan>[
+                    const TextSpan(
+                        text:
+                            'If you have any questions or would like to report a bug please mail us at '),
+                    TextSpan(
+                      text: 'idk@idk.idk',
+                      style: TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          email = "idk@idk.idk";
+                          launchMailClient();
+                        },
                     ),
-                  ),
-                ],
+                  ],
+                )),
               ),
             ),
           ],
